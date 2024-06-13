17e31702c43774ff53ddd6f0a12f574342d85a1d
h-daria <dariahalaida@gmail.com>
2024-06-13 03:52:04 +0300

    add data

---
+++ steps-page/src/app/page-layout/stepper/data.ts
@@ -0,0 +1,171 @@
+import { StepInfo } from "./steps/step-item/step-item.component";
+
+export const stepper1: StepInfo[][] = [
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: true,
+        isFinished: false
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: false,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: false,
+        isFinished: false
+        }
+    ],
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: true,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: false,
+        isFinished: false
+        }
+    ],
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: true,
+        isFinished: false
+        }
+    ],
+];
+  
+export  const stepper2: StepInfo[][] = [
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: true,
+        isFinished: false
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: false,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: false,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 4,
+        isActive: false,
+        isFinished: false
+        }
+    ],
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: true,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: false,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 4,
+        isActive: false,
+        isFinished: false
+        }
+    ],
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: true,
+        isFinished: false
+        },
+        {
+        label: 'Settings',
+        count: 4,
+        isActive: false,
+        isFinished: false
+        }
+    ],
+    [
+        {
+        label: 'Role',
+        count: 1,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Email',
+        count: 2,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Settings',
+        count: 3,
+        isActive: false,
+        isFinished: true
+        },
+        {
+        label: 'Settings',
+        count: 4,
+        isActive: true,
+        isFinished: false
+        }
+    ],
+  ];
--- steps-page/src/app/page-layout/stepper/stepper.component.ts
+++ steps-page/src/app/page-layout/stepper/stepper.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { stepper1, stepper2 } from './data';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss']
 })
-export class StepperComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class StepperComponent {
+steppersList = [stepper1, stepper2];
 }
